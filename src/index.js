f8150c51cdb39cac2b2374bf2229d1f78b09cdad
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-10-12 10:25:20 -0400

    Displays toys, allows you to add a new toy, and allows user to like a toy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+
+const toysIndexUrl = "http://localhost:3000/toys";
+const specificToyUrl = (id) => {
+  return `http://localhost:3000/toys/${id}`
+}
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -12,4 +17,88 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  getAllToys();
+  newToyListener();
 });
+
+
+const getAllToys = () => {
+  fetch(toysIndexUrl)
+  .then(resp => resp.json())
+  .then(data => {
+    data.forEach((toy) => {
+      createToyCard(toy)
+    })
+  })
+}
+
+const createToyCard = (toy) => {
+  //set variables I'll need
+  let toyCollection = document.querySelector("div#toy-collection");
+  let toyName = document.createElement("h2");
+  let toyImg = document.createElement("img");
+  let toyLikes = document.createElement("p");
+  let likeButton = document.createElement("button");
+  let toyCard = document.createElement("div");
+
+  //set the attributes on those variables
+  toyName.innerHTML = toy.name;
+  toyImg.src = toy.image;
+  toyImg.className = "toy-avatar";
+  toyLikes.innerHTML = toy.likes;
+  likeButton.className = "like-btn";
+  likeButton.innerHTML = "Like";
+  likeListener(likeButton, toy);
+  toyCard.className = "card";
+
+  //add the variables to a div with class of card
+  toyCard.append(toyName, toyImg, toyLikes, likeButton)
+
+  //add that div to the dom 
+  toyCollection.appendChild(toyCard)
+}
+
+const createNewToy = (toy) => {
+  fetch(toysIndexUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(toy)
+  })
+  .then(resp => resp.json())
+  .then(data => data)
+}
+
+const newToyListener = () => {
+  let toyForm = document.querySelector("form.add-toy-form");
+  toyForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let newToyObj = {
+      name: event.target.elements.name.value,
+      image: event.target.elements.image.value,
+      likes: "0"
+    }
+    createNewToy(newToyObj)
+  })
+}
+
+const addLike = (toy) => {
+  fetch(specificToyUrl(toy.id), {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({likes: toy.likes + 1})
+  })
+  .then(resp => resp.json())
+  .then(data => data)
+}
+
+const likeListener = (button, toy) => {
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    addLike(toy)
+  })
+}
